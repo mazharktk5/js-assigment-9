b862dc00e695f9ff680f04678b335ba3a48e6b1d
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-07-06 08:03:54 -0700

    js-assignment-9

---
+++ script.js
@@ -0,0 +1,103 @@
+
+// ===================
+// // 1. TABLE PRINT KARE
+// // DESCRIPTION: user se koi number le and uska table 10 tk print kare
+// ===================
+
+//  var inputNumber = +prompt("Please enter a number");
+
+// for (var num = 1; num <= 10; num++) {
+//   console.log(`${inputNumber} * ${num} = ${inputNumber * num}`);
+// }
+
+
+
+// // ===============================
+// // 2. ARRAY ELEMENTS KA SUM PRINT KARE
+// // // DESCRIPTION:
+// // // 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// // // 2. phr array elements ka sum find kar k print kare
+// // ================================
+
+// var numbers = [];
+
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("enter a number" );
+    
+//    var number = numbers.push(userInput)
+
+// }
+
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+
+
+// console.log("Sum of array elements:", sum);
+
+
+
+// ===============================================
+// // 3. ARRAY KA SUB SE BARA NUMBER PRINT KARE
+// // DESCRIPTION:
+// // 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// // 2. phr array elements me SUB SE BARA NUMBER find kar k print kare
+// ==================================================
+
+
+
+
+//  var numbers = [];
+
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("enter the number")
+  
+//     var number = numbers.push(userInput);
+// }
+
+// var largestNumber = numbers[0];
+
+// for (var i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > largestNumber) {
+//         largestNumber = numbers[i];
+//     }
+// }
+
+// console.log("The largest number in the array is:", largestNumber);
+
+
+// ===============================================
+// // 5. FACTORIAL
+// // // DESCRIPTION:
+// // // 1. user se koi b NUMBER input le
+// // // 2. us number ka factorial find kr k print kare
+// ===============================================
+
+
+// let userInput = prompt("Enter a number:");
+
+// let factorial = 1;
+// for (let i = 1; i <= userInput; i++) {
+//     factorial *= i;
+// }
+
+// console.log(`Factorial of ${userInput} is:`, factorial);
+
+
+
+
+// ========================================
+// 4. USER SE STRING LE AND USKO ULTA PRINT KARE
+// // DESCRIPTION:
+// // 1. user se koi b string input le
+// // 2. usko reverse me print karde e.g: mehtab -> bathem
+// ========================================
+
+
+var string = "Mehtab alam";
+var result = "";
+for (var i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+}
+console.log(result);
